77b0f5b783f9dca0d710bf8228c0ad83b11cfb86
rahmam <mohammed.rahman@cdk.com>
2017-07-31 12:52:20 +0530

    First Commit

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnetcore.Controllers
+{
+    [Route("api/products")]
+    public class ProductController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new[] { "Product 1", "Product 2" };
+        }
+    }
+}
